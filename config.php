d2dfcc4167c93a9a8787d301ef3b8d7734a3d435
ininbox-developer <developer@ininbox.com>
2014-05-07 11:15:28 +0530

    Version 1.0.1
    Updated version

--- config.php
+++ config.php
@@ -1,3 +1,7 @@
 <?
 $api_key = "e2Uq3Yk5Jk4Vf2Ov3Ma3";
+$request_format = "xml"; // xml or json
+
+define('ACTION_NOT_FOUND', 'Action not defined.');
+define('INVALID_ACTION', 'Invalid action. The action must be either %s.');
 ?>
